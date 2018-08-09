b6a7db10a09d858e2530dafa15f05bd0b1e1c415
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-09 03:24:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -195,5 +195,36 @@ client.on('message', function(message) {
 
 
 
+client.on('message', message => {
+let args = message.content.split(' ').slice(1);
+if(message.content.split(' ')[0] == 'لون'){
+if(message.channel.id !== "471966277196578818") return;
+const embedd = new Discord.RichEmbed()
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setDescription(`لايوجد لون بهذا الرقم`)
+.setColor(`ff0000`)
+if(!isNaN(args) && args.length > 0)
+if(!(message.guild.roles.find("name",`${args}`))) return  message.channel.sendEmbed(embedd);
+var a = message.guild.roles.find("name",`${args}`)
+if(!a)return;
+const embed = new Discord.RichEmbed()                    
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setDescription(`تم تغيير اللون بنجاح`)
+.setColor(`${a.hexColor}`)
+message.channel.sendEmbed(embed);
+if (!args)return;
+setInterval(function(){})
+let count = 0;
+let ecount = 0;
+for(let x = 1; x < 201; x++){
+message.member.removeRole(message.guild.roles.find("name",`${x}`))
+}
+message.member.addRole(message.guild.roles.find("name",`${args}`));
+}
+});
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);

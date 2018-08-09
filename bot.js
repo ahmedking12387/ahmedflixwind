ecdf28d5c23cf444210fd03122f55bd27b3c5cf9
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-10 02:07:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -196,14 +196,6 @@ client.on('message', function(message) {
 
 
 
-client.on('message', message => {
-    if(message.content.startsWith('+sa')) {
-    let args = message.content.split(' ').slice(1);
-    let ar = args.join(' ');
-
-    message.channel.send(ar,{tts:true});
-}
-});
 
 
 

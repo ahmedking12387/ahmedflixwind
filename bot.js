f83cbde751b48b7c714780ce8b9e6c5428fd49ad
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-09 03:15:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -165,4 +165,35 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('ليس 
 
 
 
+
+
+
+
+
+client.on('message', function(message) {
+    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('discord.gg')){
+        message.reply (' ')
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', 'BlackList')); 
+    const embed500 = new Discord.RichEmbed()
+      .setTitle(":x: | تمت معاقبتك")
+            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
+      .addField(`by`,`shyboy_05`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL) 
+        .setFooter(`${message.guild.name} Server`)
+     message.channel.send(embed500) 
+    
+        
+    }
+    }
+})
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);

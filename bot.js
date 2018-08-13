167d54bba8a9d3600120464d363d3d17ebf98fc7
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-13 19:11:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -345,4 +345,26 @@ msg.channel.send({embed: embed})
 
 
 
+
+
+client.on("message", (message) => {
+            if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        let yumz = new Discord.RichEmbed()
+                    .setTimestamp()
+                    .setTitle("Direct Message To The Bot")
+                    .addField(`Sent By:`, `<@${message.author.id}>`)
+                    .setColor("RANDOM")
+                    .setThumbnail(message.author.displayAvatarURL)
+                    .addField(`Message: `, `\n\n\`\`\`${message.content}\`\`\``)
+                    .setFooter(`DM Bot Messages | DM Logs`)
+                client.users.get("279194403564814336").send(yumz)
+            }
+});
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);

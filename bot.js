da86f33ed554adc8cdbb394f69024d043528a9db
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-23 09:18:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -466,7 +466,14 @@ client.on('message', function(message) {
     }
 });
 
-
+client.on('message',function(message) {
+	let prefix = "-";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`**# ${args}**`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
 
 
 

e52250c92b889559ad8d29321d39af74ed70ea41
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-23 06:04:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -408,33 +408,6 @@ msg.channel.send({embed: embed})
 
 
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-// -say
-  if (command === "000" {
-          message.delete()
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-  }
-  
- 
-
-if (command == "embed") {
-    let say = new Discord.RichEmbed()
-    .setDescription(args.join("  "))
-    .setColor("RANDOM")
-    message.channel.sendEmbed(say);
-    message.delete();
-  }
-
-
-});
 
 
 

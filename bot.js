62a198f221de196358d1f7978d431b3a4b11b62e
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-29 10:56:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,26 @@ client.on('ready', () => {
 });
 
 
-
+client.on("ready", () => {
+    client.user.setPresence({
+      status: 'dnd',
+      game: { 
+         type: 0,
+         name: 'Surprise Mother Fucker :) ',
+         details: `I'm : KBOOSH - Developer and Programmer`,
+         url: 'http://twitch.tv/Streammingg',
+         state: `إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ`,
+        application_id: '281376075802476544',
+         assets: {
+            small_image: `366835431037337600`,
+            small_text: ' Take This ! ' ,
+            large_image: `370451271133429760`,
+            large_text: `ҜṦẰ FOREVER ♥` }
+    
+      }
+        });
+    });
+    
 
   client.on("message", message => {
     if(message.content.startsWith(prefix + "server")) {

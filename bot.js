da71572bde6b3329c1bd9ecbb19db043c0cec48a
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-09-16 20:53:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,19 @@ client.on('ready', () => {
 });
 
 
-
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var stewart = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``رساله جديده في خاص البوت``')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("490942592301662218").send({ embed: stewart });
+    }
+});
     
 
   client.on("message", message => {

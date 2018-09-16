75252b34a8b67cbf7411b5bd26bbe51a26a83478
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-09-16 21:18:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,20 +26,6 @@ client.on('ready', () => {
   console.log('')
 });
 
-
-client.on('message', function(message) {
-    if (message.channel.type === "dm") {
-        if (message.author.id === client.user.id) return;
-        var stewart = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTimestamp()
-            .setTitle('``رساله جديده في خاص البوت``')
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
-            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
-        client.channels.get("490942592301662218").send({ embed: stewart });
-    }
-});
     
 
   client.on("message", message => {
@@ -403,8 +389,19 @@ member.addRole(KinG66S[member.user.id].roles.shift());
 
 
 
-
-
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var stewart = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``رساله جديده في خاص البوت``')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("490942592301662218").send({ embed: stewart });
+    }
+});
 
 
 client.login(process.env.BOT_TOKEN);

6cc01fb36eea964f647a343039cca41f352b1c3f
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-09-22 15:34:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,15 +26,7 @@ client.on('ready', () => {
   console.log('')
 });
 
-client.on('message', message => {
-    if(message.channel.type === 'dm') {
-        var guildID = '475600365698547712'; 
-        if(message.content.includes('discord.gg/')) {
-            var member = client.guilds.find(g => g.id === guildID).members.find(m => m.id === message.author.id);
-            member.ban({ reason: 'نشر ب الخاص' }).catch();
-        }
-    }
-});
+
 
 
 
